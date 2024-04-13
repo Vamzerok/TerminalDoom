45d86349d7e3b2ed92ec4fb9ec4e2a498548ce73
csaszy <csaszy2006@gmail.com>
2024-04-13 16:31:45 +0200

    Add project files.

---
+++ TerminalDoom/Assets/Map.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalDoom.Assets
+{
+    internal class Map
+    {
+        private short[,] _layout = 
+            {   {0,1,2,3},
+                {0,1,2,3}, 
+
+            };
+        public short[,] Layout 
+        {
+            get
+            {
+                return 
+            }
+                
+        }
+    }
+}
